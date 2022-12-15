8972e0ae286492aa5636ff457a72bb93b3126bfc
Bennyc0 <benny.chen@csedge.org>
2022-12-14 20:19:05 -0500

    time to test information.html

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def leaderboard():
 
     leaderboard.setdefault("player" + len(leaderboard), [player_name, game.score])
 
-    return render_template('leaderboard.html', leaderboard=leaderboard)
+    return render_template('leaderboard.html', score_list=leaderboard)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port="7000")
