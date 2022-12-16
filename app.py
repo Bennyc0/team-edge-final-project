c4353dabc500c1ea3dad93455f94250e2bfe5c9f
Bennyc0 <benny.chen@csedge.orggi config --global user.name Bennyc0git config --global user.email benny.chen@csedge.org>
2022-12-16 04:16:55 +0000

    finished, not much styling

--- app.py
+++ app.py
@@ -8,30 +8,16 @@ game = Game()
 def index():
     return render_template('index.html')
 
-@app.route("/information", methods=['POST'])
-def information():
+@app.route("/play", methods=['POST'])
+def play():
     global game
 
     game.play_game()
+    return render_template('score.html', score=game.score)
 
-    return render_template('information.html', score=game.score)
-
-@app.route("/leaderboard", methods=['GET', 'POST'])
-def leaderboard():
-    global game
-
-    leaderboard = {}
-    player_name = ""
-
-    try:
-        player_name = request.form['playername']
-    except:
-        return render_template('leaderboard.html', score_list=leaderboard)
-
-    if player_name != "":
-        leaderboard.setdefault("player" + str(len(leaderboard)+1), [player_name, game.score])
-
-    return render_template('leaderboard.html', score_list=leaderboard)
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port="7000")
