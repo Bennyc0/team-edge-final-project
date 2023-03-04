ba9c4b2a18a7a1e97a68b2c93011fc66a73b21fb
Bennyc0 <benny.chen@csedge.org>
2023-03-04 15:53:11 -0500

    i think i forgot to save

--- app.py
+++ app.py
@@ -10,18 +10,11 @@ game = Game()
 def index():
     return render_template('index.html')
 
-<<<<<<< HEAD
 @app.route("/information", methods=['GET','POST'])
 def information():
     global score
-=======
-@app.route("/play", methods=['POST'])
-def play():
-    global game
->>>>>>> refs/remotes/origin/main
 
     game.play_game()
-<<<<<<< HEAD
     score = game.score
 
     return render_template('information.html', score=score)
@@ -41,13 +34,6 @@ def leaderboards():
     leaderboards = get_leaderboards()
 
     return render_template('leaderboards.html', leaderboards=leaderboards)
-=======
-    return render_template('score.html', score=game.score)
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
->>>>>>> refs/remotes/origin/main
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port="7000")
